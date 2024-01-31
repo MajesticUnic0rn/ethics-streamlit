1c15f681a492c3d6371959c64be32d37a40316c6
Chris Dong <chrisdong@MacBook-Pro.local>
2024-01-31 00:31:52 -0500

    adding different pages for ethics projects

---
+++ pages/golf_hate.py
@@ -0,0 +1,8 @@
+# Import necessary libraries
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+# Page Config
+st.set_page_config(page_title="Golf Hater", page_icon="📉")
+st.sidebar.header("I hate golf")
---
+++ pages/housing_analysis.py
@@ -0,0 +1,8 @@
+# Import necessary libraries
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+# Page Config
+st.set_page_config(page_title="Homeownership", page_icon="📉")
+st.sidebar.header("Homeownership Analysis")
